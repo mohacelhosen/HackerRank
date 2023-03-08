659cb316e2b4a05ff66e9da65d80d15380477913
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-08 12:20:49 +0600

    Stdin & Stdout in java

---
+++ StdIn_Stdout.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StdIn_Stdout {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        System.out.printf("%d\n%d\n%d\n",a,b,c);
+    }
+}
